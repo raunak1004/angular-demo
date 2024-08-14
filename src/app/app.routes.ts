2596f5806bdc2e10c4b2f4380cfe7fcc21a4555b
Raunak Shaw <raunak@foxsense.io>
2024-08-14 10:49:09 +0530

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: SignInComponent, title: 'Sign In' },
+    { path: 'sign-up', component: SignUpComponent, title: 'Sign Up' },
+];
